a6f0173e45e48a41af6c27f69ba4c109431656f0
Richard Chan <richardchanme@gmail.com>
2017-05-13 00:04:34 -0700

    included map marker and correct map coordinates

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@
 var map;
   // Complete the following function to initialize the map
   function initMap() {
+    var myLatLng = {lat: 37.7749295, lng: -122.4194155};
+
     // Create a map object and specify the DOM element for display.
       map = new google.maps.Map(document.getElementById("map"), {
-      center: {lat: 37.7749, lng: 122.4194},
-      zoom: 8
+      center: myLatLng,
+      zoom: 13
     });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng,
+    title: 'Hello World!'
+  });
   }
